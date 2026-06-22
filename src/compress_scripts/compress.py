213b36b2cc4afe8277f833ed5e786c76e6500d1c
Catherine La Riviere <clarivi@umich.edu>
2026-06-22 17:47:18 -0400

    added slow compression implementation - still buggy

--- src/compress_scripts/compress.py
+++ src/compress_scripts/compress.py
@@ -20,10 +20,13 @@ import ase.io
 from ase.formula import Formula
 import json 
 import itertools
+import coxeter
 
 ### PARAMETERS 
 input_dir = "../../inputs/"
 output_dir = "../../data/IceVIII/"
+
+walltime_stop = 60 * 5 # * 60 # 1 hour in seconds
 
 ## crystallographic parameters
 types = ["O", "H"]
@@ -31,14 +34,13 @@ cif = input_dir + "cif_files/141_H2O_0.cif"
 shape_files = []
 for type in types: 
     shape_files.append(input_dir + f"shapes/shape_141_H2O_0_{type}_unit_volume_principal_frame.json")
-print(shape_files)
 
 # get number of atoms 
-atoms = ase.io.read(cif) 
+atoms = ase.io.read(cif)
 type_list = Formula(str(atoms.symbols)).count()
 
 # calculate number of each shape in initial simulation
-N_scaling = 20
+N_scaling = 100
 N_types = [] # number of each type of shape
 
 for i, type in enumerate(types): #get number
@@ -49,16 +51,15 @@ print(f"Number of particles {N}")
 
 #initialize HPMC  
 mc = hoomd.hpmc.integrate.ConvexPolyhedron()
-mc.nselect = 4 #number of trials moves per particle per timestep
+mc.nselect = 2 #number of trials moves per particle per timestep
 type_shapes = []
 for i, shape_file in enumerate(shape_files):
     with open(shape_file) as file:
 
         shape_info = json.load(file)
-        print(f"type of particle {types[i]}")
-        mc.shape[types[i]] = dict(
-            vertices = shape_info["8_vertices"]
-        )
+        mc.shape[types[i]] = dict(vertices=shape_info["8_vertices"])
+        mc.d[types[i]] = 3
+        mc.a[types[i]] = 1
 
         type_shapes.append(dict(type="ConvexPolyhedron", 
                                 rounding_radius = 0,
@@ -68,7 +69,7 @@ logger = hoomd.logging.Logger()
 logger.add(mc, quantities=["type_shapes"])
 
 #initialize square lattice 
-spacing = 3
+spacing = 5
 K = math.ceil(N ** (1/3))
 L = K * spacing 
 
@@ -90,7 +91,6 @@ typeid = []
 for i in range(len(types)):
     typeid = typeid + ([i] * N_types[i])
 
-print(f"length of typeid is {len(typeid)}")
 frame.particles.typeid = typeid 
 frame.particles.types = types
 frame.particles.type_shapes = type_shapes
@@ -110,19 +110,19 @@ simulation = hoomd.Simulation(device=cpu, seed = 1)
 simulation.operations.integrator = mc 
 simulation.create_state_from_gsd(filename = output_dir + "lattice.gsd")
 
+simulation.run(1)
+print(f"initial overlaps: {mc.overlaps}")
+print(simulation.timestep)
 ti = time.time()
 print("starting randomization")
-simulation.run(10e3)
+simulation.run(1000)
 print(f"ending randomization in {time.time()-ti}")
+print(simulation.timestep)
 
-# query mc move properties to see what it did
-print(mc.translate_moves) #tuple of accepted and rejected moves 
-print(mc.translate_moves[0] / sum(mc.translate_moves)) #acceptance ratio
+simulation.run(100)
+print("rotation acceptance ", mc.rotate_moves[0]/sum(mc.rotate_moves))
 
-print(mc.rotate_moves) # tuple of accepted and rejected moves 
-print(mc.rotate_moves[0] / sum(mc.rotate_moves))
-
-print(mc.overlaps)
+print("translation acceptance ", mc.translate_moves[0] / sum(mc.rotate_moves))
 
 hoomd.write.GSD.write(state=simulation.state, mode="wb", 
                       filename=output_dir + "random.gsd", 
@@ -130,5 +130,156 @@ hoomd.write.GSD.write(state=simulation.state, mode="wb",
 
 ### SLOW COMPRESS BOX 
 
+# calculate initial packing fraction 
+shape_volume = 0
+for i, shape_file in enumerate(shape_files):
+    with open(shape_file) as file:
+        shape_info = json.load(file)
+        shape_volume += shape_info["4_Volume"] * N_types[i]
+        print(f"shape volume {types[i]} is {shape_volume}")
+
+rho_init = shape_volume / simulation.state.box.volume
+
+print(f"initial volume fraction = {rho_init}")
+
+#set compress variables 
+initial_box = simulation.state.box 
+final_box = hoomd.Box.from_box(initial_box)
+final_volume_fraction = 0.2
+final_box.volume = shape_volume / final_volume_fraction
+
+#add move tuners 
+tune = hoomd.hpmc.tune.MoveSize.scale_solver(
+    moves=["a", "d"],
+    target=0.5,
+    trigger=hoomd.trigger.And(
+        [ 
+            hoomd.trigger.Periodic(10), 
+            hoomd.trigger.Before(simulation.timestep + 5000)
+            ]
+    ),
+    types=types
+)
+#simulation.operations.tuners.append(tune)
+
+compress = hoomd.hpmc.update.QuickCompress(trigger=hoomd.trigger.Periodic(10), target_box=final_box)
+simulation.operations.updaters.append(compress)
+
+# run code 
+print("simulation timestep before compression = ", simulation.timestep)
+print(compress.complete)
+
+simulation.run(1)
+print("initial trial move size")
+print("rotation ", mc.a["O"])
+print("translation ", mc.d["O"])
+
+t2 = time.time()
+while not compress.complete and simulation.timestep < 1e6: 
+    t1 = time.time()
+    simulation.run(100)
+    print(f"compression running...{time.time() - t1}")
+    print(f"current volume = {shape_volume / simulation.state.box.volume}")
+    print(f"time {time.time() - t2}")
+    print("rotation ", mc.a["O"])
+    print("translation ", mc.d["O"])
+
+    print("rotation acceptance = ", mc.rotate_moves[0]/sum(mc.rotate_moves))
+
+    print("translation acceptance = ", mc.translate_moves[0] / sum(mc.rotate_moves))
+
+print(f"time step: {simulation.timestep}")
+print("rotation ", mc.a["O"])
+print("translation ", mc.d["O"])
+
+if not compress.complete:
+    message = "Compression failed to complete"
+    raise RuntimeError(message)
+
+simulation.operations.updaters.remove(compress)
+
+#add in after initial compression 
+simulation.operations.tuners.append(tune)
+
+# slow compress - quick compress with equilibration 
+final_box = hoomd.Box.from_box(initial_box)
+final_volume_fraction = 0.3
+final_box.volume = shape_volume / final_volume_fraction
+
+print(f"initial volume fraction : {shape_volume / simulation.state.box.volume}")
+print(f"final goal volume fraction: {shape_volume / final_box.volume}")
+
+while simulation.state.box.volume > final_box.volume:
+    # add quick compress to 99% of volume 
+    new_box = hoomd.Box.from_box(initial_box)
+    new_box.volume = simulation.state.box.volume * 0.99 # shrink by 0.99
+    print("initial volume fraction ", shape_volume / simulation.state.box.volume) 
+    print("goal volume fraction ", shape_volume / new_box.volume)
+    compress = hoomd.hpmc.update.QuickCompress(trigger=hoomd.trigger.Periodic(10), target_box=new_box)
+    simulation.operations.updaters.append(compress)
+    
+    # run until compressed to desired volume
+    while not compress.complete: 
+        simulation.run(100)
+        print(f"current volume = {shape_volume / simulation.state.box.volume}")
+        print(f"compress completeness: {compress.complete}")
+        print(simulation.device.communicator.walltime)
+        next_walltime = simulation.device.communicator.walltime + simulation.walltime
+        if next_walltime >= walltime_stop: 
+            print("Simulation timed out")
+            break
+    
+    # check new volume (to make sure it worked) 
+    print(f"quick compress complete to {shape_volume/simulation.state.box.volume}")
+
+    print("rotation acceptance = ", mc.rotate_moves[0]/sum(mc.rotate_moves))
+
+    print("translation acceptance = ", mc.translate_moves[0] / sum(mc.rotate_moves))
+
+    # remove quick compress 
+    simulation.operations.updaters.remove(compress)
+
+    # equilibrate (run for 10,000 steps, for now) 
+    simulation.run(10_000)
+
+    # add simulation break for equilibration
+    print(simulation.device.communicator.walltime)
+    next_walltime = simulation.device.communicator.walltime + simulation.walltime
+    if next_walltime >= walltime_stop: 
+        print("Simulation timed out")
+        break
+
+rho_final = shape_volume / simulation.state.box.volume
+print(f"final volume fraction: {rho_final}")
+
+hoomd.write.GSD.write(state=simulation.state, mode="wb", filename = output_dir + "compressed.gsd", logger = logger)
 
 ### EQUILIBRATE BOX - million time steps 
+
+# floppy box
+
+boxmc = hoomd.hpmc.update.BoxMC(trigger=2, P=0.0)
+
+boxmc.aspect = {"weight": 0.7, "delta": 0.3}
+boxmc.shear = {"weight": 0.7, "delta": (0.3, 0.3, 0.3), "reduce": 0.0}
+
+boxmc_tune = hoomd.hpmc.tune.BoxMCMoveSize.scale_solver(
+    trigger=hoomd.trigger.And(
+        [
+        hoomd.trigger.Periodic(10), # Longer tune at start of run, plus a small tune on restart
+        hoomd.trigger.Before(simulation.timestep + 10_000),
+        ]
+),
+boxmc=boxmc,
+moves=["aspect", "shear_x", "shear_y", "shear_z"],
+max_move_size={
+"aspect": 0.3,
+"shear_x": 0.3,
+"shear_y": 0.3,
+"shear_z": 0},
+target=0.3,
+)
+
+logger.add(boxmc, ["shear_moves", "aspect_moves"])
+simulation.operations.updaters.append(boxmc)
+simulation.operations.tuners.append(boxmc_tune)
---
+++ src/compress_scripts/scratch.py
@@ -0,0 +1,193 @@
+### IMPORTS 
+import math 
+import hoomd 
+import gsd.hoomd 
+import freud
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import plotly.graph_objects as go
+import os 
+import time
+import ase.io
+from ase.formula import Formula
+import json 
+import itertools
+import coxeter
+
+#parameters: 
+input_dir = "../../inputs/"
+output_dir = "../../data/IceVIII/"
+cif = input_dir + "cif_files/141_H2O_0.cif"
+walltime_stop = 60 * 60 # 1 hour in seconds
+
+# get shape information
+types = ["O", "H"]
+N_scaling = 100
+
+
+def get_shape_info(N_scaling, types, cif):
+    atoms = ase.io.read(cif)
+    type_list = Formula(str(atoms.symbols)).count()
+    N_types = []
+    type_shapes = [] 
+    typeid = []
+    shapes_info = []
+    radius = []
+    for i, type in enumerate(types): 
+        shape_file = input_dir + f"shapes/shape_141_H2O_0_{type}_unit_volume_principal_frame.json"
+        N_types.append(type_list[type] * N_scaling)
+        typeid = typeid + ([i] * N_types[i])
+        with open(shape_file) as file: 
+            shapes_info.append(json.load(file))
+            type_shapes.append(dict(type="ConvexPolyhedron", 
+                                    rounding_radius = 0,
+                                    vertices = shapes_info[i]["8_vertices"]))
+            poly=coxeter.shapes.ConvexPolyhedron(shapes_info[i]["8_vertices"])
+            radius.append(poly.minimal_bounding_sphere.radius)
+    
+    spacing = 2.2 * max(radius)
+
+    return atoms, type_list, N_types, type_shapes, typeid, shapes_info, spacing
+
+
+def initialize_lattice(spacing, N, typeid, type_shapes): 
+    #initialize square lattice 
+    K = math.ceil(N ** (1/3))
+    L = K * spacing 
+
+    x = np.linspace(-L/2, L/2, K, endpoint = False)
+    position = list(itertools.product(x, repeat=3))
+    position = position[0:N]
+
+    orientation = [(1, 0, 0, 0)] * N #set orientation to all "vertical"
+
+    #write initial configuration
+    frame = gsd.hoomd.Frame()
+    frame.particles.N = N
+    frame.particles.position = position 
+    frame.particles.orientation = orientation 
+    frame.particles.typeid = typeid 
+    frame.particles.types = types
+    frame.particles.type_shapes = type_shapes
+    frame.configuration.box = [L, L, L, 0, 0, 0]
+
+    with gsd.hoomd.open(name=output_dir + "lattice.gsd", 
+                        mode="w") as f: 
+        f.append(frame)
+
+    return frame
+
+
+def initialize():
+    types = ["O", "H"]
+    N_scaling = 100
+    
+    # get shape info 
+    atoms, type_list, N_types, type_shapes, typeid, shapes_info, spacing = get_shape_info(N_scaling, types, cif)
+    N = sum(N_types)
+    print(spacing)
+    frame = initialize_lattice(spacing, N, typeid, type_shapes)
+    # initialize simulation
+    cpu = hoomd.device.CPU()
+    simulation = hoomd.Simulation(device=cpu, seed = 1)
+
+    # initialize integrator 
+    mc = hoomd.hpmc.integrate.ConvexPolyhedron()
+    mc.nselect = 2 #number of trials moves per particle per timestep
+
+    for i, type in enumerate(types):
+        mc.shape[type] = dict(vertices=shapes_info[i]["8_vertices"])
+        mc.a[type] = 100
+        mc.d[type] = 20
+
+    simulation.operations.integrator = mc 
+    simulation.create_state_from_gsd(filename = output_dir + "lattice.gsd")
+    ti = time.time()
+    simulation.run(10)
+    print(time.time() - ti)
+    print("box size is ", simulation.state.box.L)
+    print(f"oxygen rotation is {mc.a["O"]}, translation is {mc.d["O"]}")
+    print("rotation acceptance ", mc.rotate_moves[0] / sum(mc.rotate_moves))
+
+    print("translation acceptance ", mc.translate_moves[0] / sum(mc.rotate_moves))
+
+    return simulation 
+
+initialize() 
+
+def compress():
+    simulation = initialize()
+
+
+def equilibrate(): 
+    simulation = initialize()
+
+    logger = hoomd.logging.Logger()
+    logger.add(simulation.operations.integrator, 
+                quantities=["type_shapes"])
+
+    #TUNING TRIAL MOVE SIZE 
+
+    tune = hoomd.hpmc.tune.MoveSize.scale_solver(
+        moves=["a", "d"],
+        target=0.2,
+        trigger=hoomd.trigger.And(
+            [
+                hoomd.trigger.Periodic(100), 
+                hoomd.trigger.Before(simulation.timestep + 5000)
+                ]
+        ),
+    )
+    simulation.operations.tuners.append(tune)
+
+    # # FLOPPY BOX 
+    sim_time = 10000
+    log_time = int(sim_time/10)
+
+    boxmc = hoomd.hpmc.update.BoxMC(trigger=2, P=0.0)
+
+    boxmc.aspect = {"weight": 0.7, "delta": 0.3}
+    boxmc.shear = {"weight": 0.7, "delta": (0.3, 0.3, 0.3), "reduce": 0.0}
+
+    boxmc_tune = hoomd.hpmc.tune.BoxMCMoveSize.scale_solver(
+        trigger=hoomd.trigger.And(
+            [
+            hoomd.trigger.Periodic(10), # Longer tune at start of run, plus a small tune on restart
+            hoomd.trigger.Before(simulation.timestep + 10_000),
+            ]
+    ),
+    boxmc=boxmc,
+    moves=["aspect", "shear_x", "shear_y", "shear_z"],
+    max_move_size={
+    "aspect": 0.3,
+    "shear_x": 0.3,
+    "shear_y": 0.3,
+    "shear_z": 0},
+    target=0.3,
+    )
+
+    logger.add(boxmc, ["shear_moves", "aspect_moves"])
+    simulation.operations.updaters.append(boxmc)
+    simulation.operations.tuners.append(boxmc_tune)
+
+    # ADD WRITER (AFTER LOGGER IS FULLY CONSTRUCTED)
+
+    gsd_writer = hoomd.write.GSD(
+        filename=output_dir + "trajectory_temp.gsd", 
+        trigger=hoomd.trigger.Periodic(log_time), 
+        mode="xb", logger = logger)
+    simulation.operations.writers.append(gsd_writer)
+    
+    t = time.time()
+
+    print("starting equilibration...")
+
+    simulation.run(sim_time)
+    
+    print(f"ran {sim_time} steps in {time.time() - t}")
+
+    simulation.operations.writers.remove(gsd_writer) #GSD files usually close when python script completes, mostly necessary if you run in a notebook
+    del gsd_writer
+
