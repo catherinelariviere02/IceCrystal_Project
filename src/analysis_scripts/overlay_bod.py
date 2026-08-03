9c6f53662c590a94847b334bab2596bf33696406
Catherine La Riviere <clarivi@umich.edu>
2026-08-03 15:44:35 -0400

    adding analysis scripts

---
+++ src/analysis_scripts/overlay_bod.py
@@ -0,0 +1,129 @@
+# Copyright (c) 2021 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+
+import matplotlib.cm
+import matplotlib.colors
+
+import PySide6.QtGui
+from ovito.data import DataCollection
+from ovito.pipeline import Pipeline
+from ovito.vis import ViewportOverlayInterface
+from traits.api import Dict, Enum, Float, Int, Range, String, Tuple
+
+import freud
+import numpy as np
+
+
+class BondOrderOverlay(ViewportOverlayInterface):
+    # Adjustable user parameters:
+    bins = Tuple((220, 220), label="(θ,φ) Bins", dtype=tuple[int, int])
+    output_size = Range(value=440, low=1, label="Output Image Size", dtype=int)
+
+    neighbors = Dict({"r_max": 1.4}, label="Neighbor Query")
+    nframes = Int(1, label="# Frames to Average")
+
+    cmap = String(value="afmhot", label="Colormap")
+    sigma_blur = Float(0.0, label="σ_blur")
+
+    clip_percentile = Range(value=99.9, low=0.0, high=100.0, label="Clip Percentile")
+
+    mode = Enum(
+        ["bod", "obcd", "lbod", "oocd"],
+        value="bod",
+        label="BOD Mode",
+    )
+
+    # Code image position to the anchor parameter of canvas.draw_image
+    anchor = Enum(
+        ["south west", "south east", "north west", "north east"],
+        value="south east",
+        label="Image Position",
+    )
+
+    def render(
+        self,
+        canvas: ViewportOverlayInterface.Canvas,
+        data: DataCollection,
+        pipeline: Pipeline,
+        frame: int,
+        **kwargs,
+    ):
+        bod = freud.environment.BondOrder(bins=self.bins, mode=self.mode)
+        
+        if hasattr(data.particles, "orientations") and data.particles.orientations is not None: 
+            orientations = data.particles.orientations[:].tolist()
+        else:
+            orientations = None 
+            
+        for f in range(frame+1)[-self.nframes :]:
+            data = pipeline.compute(f)
+            bod.compute(
+                system=data,
+                neighbors=dict(self.neighbors),
+                orientations=orientations,
+                reset=False,
+            )
+
+        # Conver the BOD to a 2D image with the correct size and view orientation
+        view = to_view(
+            bod,
+            canvas.view_tm[:, :3],
+            image_size=self.output_size,
+            sigma_blur=self.sigma_blur,
+        )
+
+        vmin, vmax = 0.0, np.nanpercentile(view, self.clip_percentile)
+        norm = matplotlib.colors.Normalize(vmin=vmin, vmax=vmax, clip=True)
+        cmap = matplotlib.colormaps.get_cmap(self.cmap)
+        image = cmap(norm(view))
+        buf = (image * 255).astype(np.uint8)
+
+        width, height, bytes_per_pixel = buf.shape
+        img = PySide6.QtGui.QImage(
+            buf, width, height, width * bytes_per_pixel, canvas.preferred_qimage_format
+        )
+
+        size_fraction = self.output_size / np.array(canvas.logical_size)
+        image_pos = (
+            0.01 if "west" in self.anchor else 0.99,
+            0.01 if "south" in self.anchor else 0.99,
+        )
+
+        canvas.draw_image(img, pos=image_pos, size=size_fraction, anchor=self.anchor)
+
+        return
+
+
+def to_view(bod, view_matrix, image_size, sigma_blur):
+    """Convert a BOD in spherical coordinates to a hemispherical image on a plane."""
+    lin_grid = np.linspace(-1, 1, image_size)
+    x, y = np.meshgrid(lin_grid, -lin_grid)
+    r2 = x**2 + y**2
+    z = np.sqrt(np.clip(1 - r2, 0, None))
+    xyz = np.dstack((x, y, z))
+
+    xyz = xyz @ view_matrix
+    x, y, z = xyz[..., 0], xyz[..., 1], xyz[..., 2]
+
+    view = np.zeros((image_size, image_size))
+    phi = np.arccos(z)
+    theta = np.arctan2(y, x) % (2 * np.pi)
+  
+    num_theta_bins, num_phi_bins = bod.nbins
+
+    theta_bins = np.trunc(theta / (2 * np.pi) * num_theta_bins).astype(int)
+    phi_bins = np.trunc(phi / np.pi * num_phi_bins).astype(int)
+    
+ 
+    view = bod.bond_order[theta_bins, phi_bins]
+
+    view[r2 > 1] = np.nan
+    if sigma_blur > 0.0:
+        try:
+            import scipy
+        except ImportError as err:
+            raise ImportError("Scipy must be installed to use blur!") from err
+        view = scipy.ndimage.gaussian_filter(view, sigma_blur)
+
+    return view
---
+++ src/analysis_scripts/rdf_overlay.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2019 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+import matplotlib.pyplot as plt
+from ovito.data import DataCollection
+from ovito.vis import ViewportOverlayInterface
+from traits.api import Bool, Enum, Range, Tuple
+
+import freud
+import numpy as np
+
+
+class RadialDistributionOverlay(ViewportOverlayInterface):
+    # Adjustable user parameters:
+    bins = Range(value=300, low=1, label="RDF Bins", dtype=int)
+    r_max = Range(value=5.0, low=0.0, label="Maximum Radius")
+    font_scale = Range(value=2.75, low=0.0, label="Font Scale")
+
+    output_size = Tuple((512, 384), label="Output Image Size", dtype=tuple[int, int])
+
+    anchor = Enum(
+        ["south west", "south east", "north west", "north east"],
+        value="north east",
+        label="Image Position",
+    )
+
+    compute_first_shell_min = Bool(value=False, label="Compute first shell radius")
+
+    cmap = "afmhot"
+
+    def render(
+        self,
+        canvas: ViewportOverlayInterface.Canvas,
+        data: DataCollection,
+        **kwargs,
+    ):
+        rdf = freud.density.RDF(bins=self.bins, r_max=self.r_max)
+        rdf.compute(data)
+
+        image_pos = (
+            0.01 if "west" in self.anchor else 0.99,
+            0.01 if "south" in self.anchor else 0.99,
+        )
+
+        size = self.output_size / np.array(canvas.logical_size)
+
+        # Create figure
+        with canvas.mpl_figure(
+            pos=image_pos,
+            size=size,
+            anchor=self.anchor,
+            tight_layout=True,
+            font_scale=self.font_scale,
+        ) as fig:
+            ax = fig.subplots()
+            rdf.plot(ax=ax)
+            ax.set_xticks(np.arange(rdf.bin_centers[0], self.r_max + 1).astype(int))
+            ax.set_yticks([])
+
+            if self.compute_first_shell_min:
+                import scipy
+
+                rdf_minima = scipy.signal.argrelmin(rdf.rdf)[0]
+                min_point = rdf.bin_centers[rdf_minima[np.argmin(rdf.rdf[rdf_minima])]]
+                plt.vlines([min_point], *ax.get_ylim(), "k", "dashed", label=min_point.round(3))
+                plt.legend()
+                print(f"RDF_MIN: {min_point:.6f}")
